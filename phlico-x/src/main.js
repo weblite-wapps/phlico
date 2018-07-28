c6ec9d6de564f07ca26711600a6edd617e899cae
ali <aliasgary73@gmail.com>
2018-07-28 20:14:29 +0430

    chore(instalite): Add @vue/cli to phlico-x

---
+++ phlico-x/src/main.js
@@ -0,0 +1,13 @@
+// module
+import Vue from 'vue'
+// global css style
+import './helper/style/normalize.css'
+import './helper/style/main.css'
+import './helper/style/global.css'
+// components
+import App from './App'
+
+
+Vue.config.productionTip = false
+
+new Vue({ render: h => h(App) }).$mount('#app')

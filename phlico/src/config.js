e28184f77f19e66e96c7ff38deec97f7d8a99e33
ali <aliasgary73@gmail.com>
2018-11-27 15:27:11 +0330

    fix bugs and build frontends

--- phlico/src/config.js
+++ phlico/src/config.js
@@ -3,5 +3,5 @@ export default (process.env.NODE_ENV === "development"
       server: "https://localhost:3094",
     }
   : {
-      server: "https://weblite.me:3094",
+      server: "https://195.248.241.33:3094",
     })
--- server/src/helper/file.js
+++ server/src/helper/file.js
@@ -10,7 +10,7 @@ const getImagePath = fileName => path.resolve(imagesPath, fileName)
 const remove = fileName => {
   const filePath = getImagePath(fileName)
   fs.stat(filePath, err => {
-    if (!err) return fs.unlink(filePath)
+    if (!err) return fs.unlink(filePath, console.log)
     return new Error("Remove function is terminated")
   })
 }

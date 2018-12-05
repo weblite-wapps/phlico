14d96025feb6d09867b623709212092f05a7ca50
ali <aliasgary73@gmail.com>
2018-12-05 15:15:01 +0330

    build and change ssl certificate

--- phlico-x/src/config.js
+++ phlico-x/src/config.js
@@ -1,5 +1,5 @@
 export default process.env.NODE_ENV === 'development' ? {
   server: 'https://localhost:3094',
 } : {
-  server: 'https://195.248.241.33:3094',
+  server: 'https://wapp.weblite.me:3094',
 }

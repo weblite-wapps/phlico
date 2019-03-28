0389e7e518e9c6ed5899d5612cd977a3c2cae4dc
Masoud Salehi <msk.masoud.74@gmail.com>
2019-03-28 15:02:48 +0430

    chore(server): Remove SSL; Change production port; Change package name

--- server/src/app.js
+++ server/src/app.js
@@ -2,9 +2,6 @@
 const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
-const https = require("https")
-const fs = require("fs")
-const path = require("path")
 // router and db handler
 const router = require("./router/router")
 const database = require("./db/index")
@@ -15,7 +12,4 @@ app.use(cors({ origin: "*" }))
 app.use(bodyParser.json())
 app.use("/", router)
 
-const key = fs.readFileSync(path.resolve("./src/certs/express.key"), "utf8")
-const cert = fs.readFileSync(path.resolve("./src/certs/express.crt"), "utf8")
-
-https.createServer({ key, cert }, app).listen(3094)
+app.listen(3094, () => console.log('Server Running!'))
